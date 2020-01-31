e8497e93acb56ad74f9fe5956e6e987a03e86ba8
AG89Z <agiachino@zmorph3d.com>
2020-01-31 11:30:01 +0100

    Vary acc and speed based on axes limits

--- index.js
+++ index.js
@@ -46,12 +46,67 @@ const drawFrame = frame => {
 
   const position = frame.position;
   const speed = frame.speed;
+
   const target = frame.settings.path[frame.target];
 
+  // const dir = target.sub(position).unit();
+  // const nextSpeed = speed
+  //   .add(dir.scale(frame.settings.acceleration.x))
+  //   .limit(frame.settings.maxSpeed.x);
+  // const nextPosition = position.add(nextSpeed);
+  // const nextTarget =
+  //   target.sub(nextPosition).mag() < frame.settings.maxSpeed.x
+  //     ? (frame.target + 1) % frame.settings.path.length
+  //     : frame.target;
   const dir = target.sub(position).unit();
-  const nextSpeed = speed
-    .add(dir.scale(frame.settings.acceleration.x))
-    .limit(frame.settings.maxSpeed.x);
+
+  const axesAcceleration = frame.settings.acceleration;
+  var acceleration;
+  if (
+    Math.abs(dir.y / dir.x) > Math.abs(axesAcceleration.y / axesAcceleration.x)
+  ) {
+    acceleration = Math.abs(axesAcceleration.y / dir.y);
+  } else {
+    acceleration = Math.abs(axesAcceleration.x / dir.x);
+  }
+
+  // console.log(acceleration);
+  const accelerationVec = dir.scale(acceleration);
+
+  // const acceleration =
+  //   dir.x * axesAcceleration.y < axesAcceleration.x
+  //     ? axesAcceleration.y
+  //     : axesAcceleration.x;
+  // var acceleration = Math.abs(Math.min(
+  //   axesAcceleration.x / (dir.x + 0.000000001),
+  //   axesAcceleration.y / (dir.y + 0.000000001)
+  // ));
+
+  const speedT = speed.add(accelerationVec);
+  var maxSpeed = 100000;
+  if (
+    Math.abs(speedT.x) > frame.settings.maxSpeed.x &&
+    Math.abs(speedT.y) < frame.settings.maxSpeed.y
+  ) {
+    maxSpeed = frame.settings.maxSpeed.x;
+  } else if (
+    Math.abs(speedT.y) > frame.settings.maxSpeed.y &&
+    Math.abs(speedT.x) < frame.settings.maxSpeed.x
+  ) {
+    maxSpeed = frame.settings.maxSpeed.y;
+  } else if (
+    Math.abs(speedT.x) > frame.settings.maxSpeed.x &&
+    Math.abs(speedT.y) > frame.settings.maxSpeed.y
+  ) {
+    maxSpeed =
+      frame.settings.maxSpeed.x > frame.settings.maxSpeed.y
+        ? frame.settings.maxSpeed.y
+        : frame.settings.maxSpeed.x;
+  }
+
+  // console.log(maxSpeed);
+
+  const nextSpeed = speedT.limit(maxSpeed);
   const nextPosition = position.add(nextSpeed);
   const nextTarget =
     target.sub(nextPosition).mag() < frame.settings.maxSpeed.x
@@ -69,7 +124,6 @@ const drawFrame = frame => {
 };
 
 const draw = frame => () => {
-
   const newFrame = drawFrame(frame);
 
   window.requestAnimationFrame(draw(newFrame));
@@ -83,8 +137,8 @@ const settings = settingsData({
     V.new(200, 100),
     V.new(100, 100)
   ],
-  acceleration: V.new(1, 1),
-  maxSpeed: V.new(3, 3)
+  acceleration: V.new(0.1, 1),
+  maxSpeed: V.new(4, 2)
 });
 
 window.requestAnimationFrame(
