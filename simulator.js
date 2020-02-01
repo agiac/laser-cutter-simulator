ab413d7b7ce559d76572dd3f05c003d3be5f709f
AG89Z <agiachino@zmorph3d.com>
2020-02-01 12:49:04 +0100

    Setup for segment planning

--- simulator.js
+++ simulator.js
@@ -1,21 +1,37 @@
 import Vector from "./vector";
 
-export function plan(path, settings, start) {
-  //Each points of the path has {location, desiredSpeed}. Settings have {maxSpeedXY, accelerationXY}
-  var res = [];
-  var current = start;
-  for (var i = 0; i < path.length; i++) {
-    const target = path[i].position;
-    const targetSpeed = path[i].desiredSpeed;
-    res.push({
-      start: current,
-      target: target,
-      speed: targetSpeed,
-      acceleration: 0
-    });
-    current = target;
-  }
-  return res
-} //return list of speed points ({position, direction, speed, acceleration} along the path)
+function calculateTargetSpeeds(path, settings, start) {
+  return path.map(({ position, desiredSpeed }) => ({
+    position,
+    targetSpeed: 100
+  }));
+}
 
-export function estimateTime(speedPoints) {} //return estimated total time
+function planSegment(start, target, settings) {
+  //1 Find deceleration point
+  //2 Find stop accelerating point
+  return [
+    {
+      start: start.position,
+      target: target.position,
+      speed: target.targetSpeed,
+      acceleration: 0
+    }
+  ];
+}
+
+export function plan(path, settings, startPosition) {
+  const targetSpeeds = calculateTargetSpeeds(path, settings, start);
+
+  var result = [];
+  var start = { position: startPosition, speed: 0 };
+
+  targetSpeeds.forEach(target => {
+    result.push(...planSegment(start, target, settings));
+    start = { position: target.position, speed: target.targetSpeed };
+  });
+
+  return result;
+}
+
+export function estimateTime(speedPoints) {}
