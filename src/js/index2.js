398410aa1d6f65caa2de2df1d3c1523a9542c4af
AG89Z <agiachino@zmorph3d.com>
2020-02-11 10:25:21 +0100

    Setup redux

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,5 +1,6 @@
 import * as R from "ramda";
 import * as Simulator from "./simulator";
+import { combineReducers, createStore } from "redux";
 
 // UTILS
 
@@ -13,8 +14,73 @@ const addOnChangeEventListener = addEventListener("change");
 
 const setInLocalStorage = (key, value) => (localStorage[key] = value);
 const getFromLocalStorage = key => localStorage[key];
+const numberFromLocalStorage = R.pipe(getFromLocalStorage, parseFloat);
 
 const setValueImpure = R.curry((key, value, obj) => (obj[key] = value));
+
+// ACTIONS TYPES
+
+const CHANGE_SETTING = "CHANGE_SETTING";
+const CHANGE_PATH = "CHANGE_PATH";
+
+// ACTIONS CREATORS
+
+const changeSetting = ({ setting, value }) => ({
+  type: CHANGE_SETTING,
+  change: { setting, value }
+});
+
+const changePath = path => ({
+  type: CHANGE_PATH,
+  path
+});
+
+// REDUCERS
+
+
+const initialSettingsState = {
+  maximumSpeedX: numberFromLocalStorage("maximumSpeedX") || 500,
+  maximumSpeedY: numberFromLocalStorage("maximumSpeedY") || 500,
+  accelerationX: numberFromLocalStorage("accelerationX") || 3000,
+  accelerationY: numberFromLocalStorage("accelerationY") || 3000,
+  minimumJunctionSpeed: numberFromLocalStorage("minimumJunctionSpeed") || 0,
+  junctionDeviation: numberFromLocalStorage("junctionDeviation") || 0.01,
+  cuttingSpeed: numberFromLocalStorage("cuttingSpeed") || 100,
+  travelSpeed: numberFromLocalStorage("travelSpeed") || 400
+};
+
+const settingsReducer = (state = initialSettingsState, action) => {
+  switch (action.type) {
+    case CHANGE_SETTING:
+      return R.assoc(action.change.setting, action.change.value, state);
+    default:
+      return state;
+  }
+};
+
+const pathReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHANGE_PATH:
+      return action.path;
+    default:
+      return state;
+  }
+};
+
+const appReducer = combineReducers({ settings: settingsReducer, path: pathReducer });
+
+// STORE
+
+const store = createStore(appReducer);
+
+const unsubscrive = store.subscribe(e => console.log(e));
+
+store.dispatch(changePath([1,2,3]));
+store.dispatch(changeSetting({setting: 'maximumSpeedX', value: 123}))
+
+
+
+
 
 // ---
 
@@ -97,7 +163,9 @@ const setupFileHandling = fileUploadInputId => {
 
 setupFileHandling(fileUploadInputId);
 
-// Display 
+// Display
 
-getElementByIdAndApply(addEventListener('simulation', () => console.log("Simulation event")), 'time-estimation')
-
+getElementByIdAndApply(
+  addEventListener("simulation", () => console.log("Simulation event")),
+  "time-estimation"
+);
